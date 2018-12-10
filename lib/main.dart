79533e51064ee80daf546973c4a6e43dc2283c5e
Ovidio R. Calvet <calveto@gmail.com>
2018-12-10 16:48:06 -0500

    Rotating square

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,65 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(CalendarApp());
 
-class CalendarApp extends StatelessWidget {
+class CalendarApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CalendarAppState();
+  }
+}
+
+class CalendarAppState extends State<CalendarApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController animation;
+  final Tween<double> ratationTween = Tween<double>(begin: 0.0, end: 2 * pi);
+  Color color = Colors.redAccent;
+  List<Color> colors = [
+    Colors.redAccent,
+    Colors.amberAccent,
+    Colors.yellowAccent,
+    Colors.deepOrangeAccent
+  ];
+  Matrix4 rotaionTransform =  Matrix4.identity();
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      duration: Duration(milliseconds: 1800),
+      vsync: this,
+    )
+    ..repeat();
+    var rand = Random(2356);
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      this.setState(() {
+        color = colors[rand.nextInt(4)];
+        rotaionTransform = Matrix4.rotationZ(ratationTween.evaluate(animation));
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.lightGreen,
+        color: Colors.lightBlue,
         child: Stack(
           alignment: Alignment.topLeft,
           children: <Widget>[
+            Positioned(
+                left: 100,
+                top: 120,
+                child: AnimatedContainer(
+                  curve: Curves.ease,
+                  transform: rotaionTransform,
+                  color: color,
+                  width: 50,
+                  height: 50,
+                  duration: Duration(seconds: 1),
+                )),
             Positioned(
               left: 20,
               bottom: 30,
