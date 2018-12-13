3716c40b1659d72dd0a53846c6e0da263a044ce4
Ovidio R. Calvet <calveto@gmail.com>
2018-12-12 22:20:47 -0500

    Switch to page view display

--- lib/home.dart
+++ lib/home.dart
@@ -6,47 +6,60 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var format = new intl.DateFormat.yMMMd();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calendar App'),
-      ),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 150.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('${format.format(DateTime.now())}'),
-            ),
+        appBar: AppBar(
+          title: Text('Calendar App'),
+        ),
+        body: PageView.builder(
+          controller: PageController(
+            initialPage: 356
           ),
-          // Container(
-          //   alignment: Alignment.center,
-          //   width: MediaQuery.of(context).size.width,
-          //   height: 60,
-          //   decoration: BoxDecoration(
-          //     color: Colors.lightBlue,
-          //   ),
-          //   child: Text('${format.format(DateTime.now())}',
-          //       textDirection: TextDirection.ltr,
-          //       style: TextStyle(
-          //         color: Colors.black87,
-          //         fontSize: 20,
-          //         fontWeight: FontWeight.bold,
-          //       )),
-          // ),
-          SliverFixedExtentList(
-            itemExtent: 50.0,
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: Text('list item $index'),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-    );
+          itemBuilder: (BuildContext context, int index) {
+            print(index.toString());
+            return Container(
+              child: Text(
+                index.toString(),
+              ),
+            );
+          },
+        )
+        // CustomScrollView(
+        //   slivers: <Widget>[
+        //     SliverAppBar(
+        //       pinned: true,
+        //       expandedHeight: 150.0,
+        //       flexibleSpace: FlexibleSpaceBar(
+        //         title: Text('${format.format(DateTime.now())}'),
+        //       ),
+        //     ),
+        //     // Container(
+        //     //   alignment: Alignment.center,
+        //     //   width: MediaQuery.of(context).size.width,
+        //     //   height: 60,
+        //     //   decoration: BoxDecoration(
+        //     //     color: Colors.lightBlue,
+        //     //   ),
+        //     //   child: Text('${format.format(DateTime.now())}',
+        //     //       textDirection: TextDirection.ltr,
+        //     //       style: TextStyle(
+        //     //         color: Colors.black87,
+        //     //         fontSize: 20,
+        //     //         fontWeight: FontWeight.bold,
+        //     //       )),
+        //     // ),
+        //     SliverFixedExtentList(
+        //       itemExtent: 50.0,
+        //       delegate: SliverChildBuilderDelegate(
+        //         (BuildContext context, int index) {
+        //           return Container(
+        //             alignment: Alignment.center,
+        //             color: Colors.lightBlue[100 * (index % 9)],
+        //             child: Text('list item $index'),
+        //           );
+        //         },
+        //       ),
+        //     )
+        //   ],
+        // ),
+        );
   }
 }
